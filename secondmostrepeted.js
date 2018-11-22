ec54d91af179b54126d37361e9e4cd9132dce71a
mohammad rafee <mohammad.rafee@macys.com>
2018-11-22 12:18:29 +0530

    updated second Most frequent algo

--- secondmostrepeted.js
+++ secondmostrepeted.js
@@ -9,13 +9,20 @@ const findsecond = (arr) =>{
             obj[arr[i]] = 1;
         }
     }
+    first = Object.keys(obj)[0];
+    second = Object.keys(obj)[1];
     for(var key in obj){
         if(obj[key]>obj[first]){
-            first = key;
             second = first;
+            first = key;
         }
-        else if(){
-
+        else if(obj[key]>obj[second] && obj[key]!=obj[first]){
+            second = key;
         }
     }
+    return second;
 }
+
+var str = "abababb";
+//var str = "abcd";
+console.log("Second MF Charater is : ",findsecond(str));
